47f89def5d4ea08f53dc5a0cd447646469895d99
John <jasiek.ole@gmail.com>
2021-05-01 12:01:38 +0200

    First commit, setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyTimer',
+    version='1.0',
+    author='Jan Olenski',
+    license='DoWhateverUWant0.0',
+    description='This lib contains advanced timer',
+    packages=find_packages()
+)
+
